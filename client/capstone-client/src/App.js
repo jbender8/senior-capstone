1ccdd916d176301c489a739c15f037edc24d3656
Andrew G Burkus <da8urk@gmail.com>
2020-02-04 01:52:07 -0600

    added stock vis

--- client/capstone-client/src/App.js
+++ client/capstone-client/src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import Home from './Home.js';
 import Search from './Search.js';
+import {Vis1} from './Visualizations.js';
+
 
 // import db, {getAllJobs} from './util/firebase.js';
 
 function App() {
   return(
-    <Search />
+    <div>
+        <Vis1 />
+       <Search />
+    </div>
   );
 }
 
