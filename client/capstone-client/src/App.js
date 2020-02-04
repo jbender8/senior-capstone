59c36e838dd55f02f751b0c76e0e254d6de009e7
burkus <andrew@burkus.xyz>
2020-02-03 22:49:26 -0600

    added inputs and firebase working

--- client/capstone-client/src/App.js
+++ client/capstone-client/src/App.js
@@ -1,45 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Home from './Home.js';
-import {
-  Container,
-  Typography,
-  Link
-} from '@material-ui/core';
+import Search from './Search.js';
 
-const Panel = ({site, link}) => {
-  return (
-    <Container maxWidth="sm">
-      <Typography color="secondary" variant="p">
-        {site}
-      </Typography>
-      <p>
-        <Link component="button" href={link} variant="body2">
-          {site}
-        </Link>
-      </p>
-    </Container>
-  );
-}
+// import db, {getAllJobs} from './util/firebase.js';
 
 function App() {
-  const sites = [{
-                  site: 'indeed.com',
-                  link: 'https://indeed.com/'
-                },
-                {
-                  site: 'monster.com',
-                  link: 'https://monster.com/'
-                }
-              ]
-  return (
-    <div className="App">
-      <Home />
-      {
-        sites.map(site => <Panel {...site} />)
-      }
-    </div>
+  return(
+    <Search />
   );
 }
 
--- client/capstone-client/src/Home.js
+++ client/capstone-client/src/Home.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import {Typography, Container} from '@material-ui/core';
+import {Typography, Container, Grid} from '@material-ui/core';
 
 export default class Home extends React.Component {
-
     render() {
         return (
-            <Container maxWidth="lg">
-                <Typography color="primary" variant="h1">
-                    Uber Super Job Search 100000000
-                </Typography>
+            <Container maxWidth="xl">
+                <Grid container direction='row' alignItems='center' justify='center' nowrap>
+                    <Grid item>
+                        <Typography color="body1" variant="h1" align='center'>
+                            Job Search
+                        </Typography>
+                    </Grid>
+                    
+                </Grid>
             </Container>
         );
     }
