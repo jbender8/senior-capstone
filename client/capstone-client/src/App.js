9eabb1acbb0968d985df14b17c38ff834b5ce1b3
Jackson Hurst <jacksonionh@gmail.com>
2020-02-16 13:48:53 -0600

    added two examples of data visualization with fake data

--- client/capstone-client/src/App.js
+++ client/capstone-client/src/App.js
@@ -2,17 +2,38 @@ import React from 'react';
 import './App.css';
 import Home from './Home.js';
 import Search from './Search.js';
-import {Vis1} from './Visualizations.js';
+import {Vis1, Vis2} from './Visualizations.js';
 
 
 import {getAllJobs} from './util/firebase.js';
 
 
+
+
 function App() {
-  const jobs = getAllJobs();
-  return(
+  //const jobs = getAllJobs();
+  const jobs = [];
+  jobs.push({title : 'Data Analyst', salary : '73000'});
+  jobs.push({title : 'QA Analyst', salary : '68000'});
+  jobs.push({title : 'Software Engineer', salary : '81500'});
+  jobs.push({title : 'Data Scientist', salary : '79200'});
+
+  const cities = [
+    { name: 'Chicago', value: 178 }, { name: 'Houston', value: 129 },
+    { name: 'New York City', value: 203 }, { name: 'San Francisco', value: 247 },
+  ];
+  
+
+
+  //for (let i = 0; i < jobs.length; i++) {
+    //data01.push(name: jobs[i][""])
+  //}
+
+  
+return(
     <div>
-        <Vis1 />
+        <Vis1 data={jobs}/>
+        <Vis2 data={cities}/>
        <Search />
     </div>
   );
--- client/capstone-client/src/Visualizations.js
+++ client/capstone-client/src/Visualizations.js
@@ -1,28 +1,45 @@
 import React from 'react';
 import {
-    PieChart, Pie, Legend, Tooltip,
+    Bar, BarChart, Legend, Tooltip, XAxis, YAxis, PieChart, Pie, Cell
 } from 'recharts';
   
 
 
-const data01 = [
-    { name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 }, { name: 'Group D', value: 200 },
-    { name: 'Group E', value: 278 }, { name: 'Group F', value: 189 },
-  ];
-  
-  const data02 = [
-    { name: 'Group A', value: 2400 }, { name: 'Group B', value: 4567 },
-    { name: 'Group C', value: 1398 }, { name: 'Group D', value: 9800 },
-    { name: 'Group E', value: 3908 }, { name: 'Group F', value: 4800 },
-  ];
-
-
 export const Vis1 = ({data}) => {
     return (
-        <PieChart width={730} height={250}>
-            <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
-            <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
-        </PieChart>
+        <BarChart width={600} height={200} data={data}>
+            <XAxis dataKey="title" />
+		    <YAxis />
+		    <Bar dataKey="salary" fill="#8884d8" />
+	    </BarChart>
     );
+}
+  
+  const data02 = [
+    { name: 'A1', value: 100 },
+    { name: 'A2', value: 300 },
+    { name: 'B1', value: 100 },
+    { name: 'B2', value: 80 },
+    { name: 'B3', value: 40 },
+    { name: 'B4', value: 30 },
+    { name: 'B5', value: 50 },
+    { name: 'C1', value: 100 },
+    { name: 'C2', value: 200 },
+    { name: 'D1', value: 150 },
+    { name: 'D2', value: 50 },
+  ];
+
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+export const Vis2 = ({data}) => {
+    return (
+        <PieChart width={400} height={400}>
+          <Pie data={data} dataKey="value" cx={200} cy={200} outerRadius={60} fill="#8884d8" label>
+          {
+            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
+          }
+          </Pie>
+        <Legend />
+        </PieChart>
+      );
 }
