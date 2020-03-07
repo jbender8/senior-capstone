59ee45bc64d55a5ded031d20bd7b55d1cc91a7bc
bfong6 <brianfong34@hotmail.com>
2020-03-07 12:37:55 -0600

    Dice scraper done.

--- client/capstone-client/src/dice/DiceScraper.py
+++ client/capstone-client/src/dice/DiceScraper.py
@@ -88,7 +88,7 @@ class DiceScraper():
         '''push scraped data to Firestore'''
         
         docId = str(now) + ' Dice{0:0>4}'.format(self.count)
-        doc = self.fs.document('diceTest/' + docId)
+        doc = self.fs.document('ScrapedJobs/' + docId)
         doc.create(data)
 
     def scrape(self, link):
